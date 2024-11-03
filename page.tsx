6b9c520e5e4c961d47184da1dfdb4c4bf57e3087
Rahul Chaudhari <101958179+rjaswal1634@users.noreply.github.com>
2024-11-03 08:37:09 -0600

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,10 +1,5 @@
 import Feature from "@/app/home-page/features"
 import Hero from "@/app/home-page/hero"
-<<<<<<< HEAD
-=======
-// import { SmartFridgeDashboard } from "@/app/user-home/page"
-import { useUser } from "@auth0/nextjs-auth0/client"
->>>>>>> 8f07f48043f46a986078e04a03d98704423c53fc
 import { Container, Main, Section } from "@/components/craft" // Update this import to the correct path
 import Footer from "@/app/home-page/footer"
 
