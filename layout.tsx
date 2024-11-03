1db5eb62d7076f1e6916f99ff7c6e66808d71dab
Rahul Chaudhari <101958179+rjaswal1634@users.noreply.github.com>
2024-11-03 02:27:54 -0500

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/themes"
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import localFont from "next/font/local";
+import "./globals.css";
+
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "mealmate",
+  description: "your meal planning companion",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <UserProvider>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          >
+          {children}
+        </ThemeProvider>
+      </body>
+      </UserProvider>
+    </html>
+  );
+}
