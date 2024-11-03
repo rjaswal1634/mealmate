351fc4de8e7bff14ded7211c7c1af637f710d2e5
Rahul Chaudhari <101958179+rjaswal1634@users.noreply.github.com>
2024-11-03 08:36:38 -0600

    Update schedule_config.ts

--- components/schedule_config.ts
+++ components/schedule_config.ts
@@ -1,9 +1,9 @@
-const admin = require('firebase-admin');
+import admin from 'firebase-admin';
 // Initialize Firebase Admin SDK with service account
-const serviceAccount = require("./firebase-key.json");
-
+import serviceAccount from "./firebase-key.json";
+import { ServiceAccount } from 'firebase-admin';
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount as ServiceAccount),
   databaseURL: "https://food-detector-134f5-default-rtdb.firebaseio.com" // Replace with your database URL
 });
 
